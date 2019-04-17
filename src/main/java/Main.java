033775333ae0a01a05c44e29b16475da6ea64466
Roman Bulinski <aboandrus@gmail.com>
2019-04-17 15:13:54 +0200

    Implemented code with tests.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,20 +3,27 @@ public class Main {
 
     public int reverseNumb( int num){
 
-        String numbString = String.valueOf(num);
+        if( num > 0 ){
+            String mirrornumb = getString(num);
+            return Integer.valueOf( mirrornumb );
 
-        char[] stringDigit = numbString.toCharArray();
-        String mirrornumb = "";
-
-        for( int i = stringDigit.length-1; i>=0; i--  ){
-            mirrornumb = mirrornumb + stringDigit[i]   ;
+        }else{
+            num = num * -1;
+            String mirrornumb = getString(num);
+            return Integer.valueOf( "-"+mirrornumb );
         }
-
-        return Integer.valueOf( mirrornumb );
 
     }
 
-
+    private String getString(int num) {
+        String numbString = String.valueOf(num);
+        char[] stringDigit = numbString.toCharArray();
+        String mirrornumb = "";
+        for (int i = stringDigit.length - 1; i >= 0; i--) {
+            mirrornumb = mirrornumb + stringDigit[i];
+        }
+        return mirrornumb;
+    }
 
 
 }
